967be391d186da3977f34db1170842e56f764002
Ramon G. Gonze <ramongonze@gmail.com>
2021-05-06 18:04:31 -0300

    Copied graphics and information modules from the old code.

---
+++ src/graphics.cpp
@@ -0,0 +1,76 @@
+#include "graphics.h"
+
+Point::Point(){
+	this->x = 0.0f;
+	this->y = 0.0f;
+}
+
+Point::Point(long double x, long double y){
+	this->x = x;
+	this->y = y;
+}
+
+Point dist2Bary(Distribution &prior){
+	Point p;
+	p.x = prior.prob[2] + prior.prob[0]/2.0f;
+	p.y = prior.prob[0];
+	return p;
+}
+
+Point dist2Bary(long double x1, long double x2, long double x3){
+	Point p;
+	p.x = x3 + x1/2.0f;
+	p.y = x1;
+	return p;
+}
+
+bool bary2Dist(Point p, vector<long double> &prob){
+	vector<long double> prob_aux(3);
+	prob_aux[0] = p.y;
+	prob_aux[1] = 1 - p.x - p.y/2.0f;
+	prob_aux[2] = p.x - p.y/2.0f;
+
+	// Fix bug of approximation in function Information::adjustPrior()
+	for(int i = 0; i < 3; i++) if(-1e-6 <= prob_aux[i] && prob_aux[i] <= 1e-6) prob_aux[i] = 0.0f;
+
+	if(Distribution::isDistribution(prob_aux)){
+		prob[0] = prob_aux[0];
+		prob[1] = prob_aux[1];
+		prob[2] = prob_aux[2];
+		return true;
+	}
+
+	return false;
+}
+
+long double euclidianDistance(Point a, Point b){
+	return sqrt((b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y));
+}
+
+long double euclidianDistance(Point a, Vector2 b){
+	return sqrt((b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y));
+}
+
+Point pixel2Bary(double x, double y, vector<Vector2> &TrianglePoints){
+	Point p;
+
+	long double triangleEdge = TrianglePoints[2].x - TrianglePoints[1].x;
+	long double triangleHeight = TrianglePoints[1].y - TrianglePoints[0].y;
+
+	p.x = (x - TrianglePoints[1].x) / triangleEdge;
+	p.y = 1 - ((y - TrianglePoints[0].y) / triangleHeight);
+
+	return p;
+}
+
+Point bary2Pixel(double x, double y, vector<Vector2> &TrianglePoints){
+	Point p;
+	
+	long double triangleEdge = TrianglePoints[2].x - TrianglePoints[1].x;
+	long double triangleHeight = TrianglePoints[1].y - TrianglePoints[0].y;
+
+	p.x = TrianglePoints[1].x + (x * triangleEdge);
+	p.y = TrianglePoints[1].y - (y * triangleHeight);
+
+	return p;
+}
---
+++ src/graphics.h
@@ -0,0 +1,88 @@
+#ifndef _qif_graphics
+#define _qif_graphics
+
+#include "../libs/qif/qif.h"
+#include "raylib.h"
+
+using namespace std;
+
+// Errors *****************************************************************************************/
+#define NO_ERROR		0
+#define INVALID_CHANNEL 1
+#define INVALID_PRIOR	2 
+#define INVALID_VALUE	3 // i.e. "1/$2"
+
+// Settings
+#define PROB_PRECISION 3 // Precision of float numbers (# digits after .)
+#define WINDOWS_WIDTH 750
+#define WINDOWS_HEIGHT 630
+
+// Prior probability distribution radius (in pixels) **********************************************/
+#define PRIOR_RADIUS 30
+
+class Point{
+	public:
+		long double x;
+		long double y;
+
+		Point();
+		Point(long double x, long double y);
+};
+
+typedef struct Circle{
+	Point center; // Pixel coordinates
+	float radius;
+}Circle;
+
+/* Transforms a probability distribution on 3 elements in a barycentric coordinate
+ * Parameters:
+ * 		prior: Probability distribution
+ *
+ * Return: A 'Point' structure containing a barycentric coordinate.
+ */
+Point dist2Bary(Distribution &prior);
+
+/* Transforms a probability distribution on 3 elements in a barycentric coordinate
+ * Parameters:
+ * 		x1: Probability of the first element
+ * 		x2: Probability of the second element
+ * 		x3: Probability of the third element
+ *
+ * Return: A 'Point' structure containing a barycentric coordinate.
+ */
+Point dist2Bary(long double x1, long double x2, long double x3);
+
+/* Transforms a barycentric coordiante in a probability distribution on 3 elements.
+ * Parameters:
+ *		p: 	  Point containing a barycentric coordinate
+ *		prob: A vector which will receive the new probability distribution
+ *
+ * Return: true if a probability distribution was succesfully generated or false otherwise.
+ */
+bool bary2Dist(Point p, vector<long double> &prob);
+
+// Euclidian distance between two points
+long double euclidianDistance(Point a, Point b);
+long double euclidianDistance(Point a, Vector2 b);
+
+/* Transforms a pixel coordinate in barycentric coordinate
+ * Parameters:
+ *		x: value (in pixels) in x-axis
+ *		y: value (in pixels) in y-axis
+ *		TrianglePoints: values (in pixels) of triangle points
+ *
+ * Return: A 'Point' structure containing a barycentric coordinate.
+ */
+Point pixel2Bary(double x, double y, vector<Vector2> &TrianglePoints);
+
+/* Transforms barycentric coordinate in pixel coordinate
+ * Parameters:
+ *		x: value (in barycentric coordinate) in x-axis
+ *		y: value (in barycentric coordinate) in y-axis
+ *		TrianglePoints: values (in pixels) of triangle points
+ *
+ * Return: A 'Point' structure containing a barycentric coordinate.
+ */
+Point bary2Pixel(double x, double y, vector<Vector2> &TrianglePoints);
+
+#endif
